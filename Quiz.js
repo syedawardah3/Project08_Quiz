e9808ded9430a2f7b1e2830e4642f564b257657b
unknown <syedawardah3@gmail.com>
2024-05-18 01:03:35 +0500

    work complete

--- Quiz.js
+++ Quiz.js
@@ -1,37 +1,158 @@
 #! /usr/bin/env node
-import chalk from "chalk";
 import inquirer from "inquirer";
-const apiLink = "https://opentdb.com/api.php?amount=6&category=18&difficulty=easy&type=multiple";
-let fetchData = async (data) => {
-    let fetchQuiz = await fetch(data);
-    let res = await fetchQuiz.json();
-    return res.results;
-};
-let data = await fetchData(apiLink);
-let startQuiz = async () => {
-    let score = 0;
-    // for user name
-    let name = await inquirer.prompt({
+import chalk from "chalk";
+const user = await inquirer.prompt([
+    {
+        name: "name",
         type: "input",
-        name: "fname",
-        message: "What is Your Name?"
-    });
-    for (let i = 1; i < 5; i++) {
-        let answers = [...data[i].incorrect_answers, data[i].correct_answer];
-        let ans = await inquirer.prompt({
-            type: "list",
-            name: "quiz",
-            message: data[i].question,
-            choices: answers.map((val) => val),
-        });
-        if (ans.quiz == data[i].correct_answer) {
-            ++score;
-            console.log(chalk.bold.italic.blue("Correct"));
-        }
-        else {
-            console.log(`Correct answer is ${chalk.bold.italic.red(data[i].correct_answer)}`);
-        }
-    }
-    console.log(`Dear ${chalk.greenBright.bold(name.fname)}, Your Score is ${chalk.red.bold(score)}, Out of ${chalk.red.bold('5')}`);
-};
-startQuiz();
+        message: chalk.greenBright.bold("What's your name?")
+    },
+]);
+console.log(chalk.yellowBright.underline(`\t\tWelcome ${user.name} to the TypeScript Quiz!`));
+console.log(chalk.yellowBright.underline("\t\tPlease answer the following questions to the best of your ability."));
+console.log(chalk.yellowBright.underline("\t\tLet's start the quiz!"));
+const quiz = await inquirer.prompt([
+    {
+        name: "question_1",
+        type: "list",
+        message: chalk.blueBright.bold("Q1.What is the file extension for TypeScript files?"),
+        choices: [".js", ".ts", ".html", ".css"] // .ts
+    },
+    {
+        name: "question_2",
+        type: "list",
+        message: chalk.blueBright.bold("Q2.TypeScript code is compiled into:"),
+        choices: ["Machine code", "Assembly code", "JavaScript", "TypeScript bytecode"] // Javascript
+    },
+    {
+        name: "question_3",
+        type: "list",
+        message: chalk.blueBright.bold("Q3.Which tool is commonly used to transpile TypeScript code to JavaScript?"),
+        choices: ["Babel", "Webpack", "TypeScript Compiler (tsc)", "Node.js"] // TypeScript Compiler (tsc)
+    },
+    {
+        name: "question_4",
+        type: "list",
+        message: chalk.blueBright.bold("Q4.Which of these are supported in TypeScript?"),
+        choices: ["Number", "Integar", "Any", "Both A and C", "All of the above"] // Both A and C
+    },
+    {
+        name: "question_5",
+        type: "list",
+        message: chalk.blueBright.bold("Q5.Consider the code (let age = 25;). What is the data type of the variable age?"),
+        choices: ["Number", "Any", "String", "boolean"] // Number
+    },
+    {
+        name: "question_6",
+        type: "list",
+        message: chalk.blueBright.bold("Q6.Consider the code (let myName= 'kanwal bilal';). What is the data type of the variable myName? "),
+        choices: ["Number", "Any", "String", "boolean"] // string
+    },
+    {
+        name: "question_7",
+        type: "list",
+        message: chalk.blueBright.bold("Q7.Consider the code (let isMarried = true;). What is the data type of the variable isMarried? "),
+        choices: ["Number", "Any", "String", "boolean"] // boolean
+    },
+    {
+        name: "question_8",
+        type: "list",
+        message: chalk.blueBright.bold("Q8.Typescript is the superset of? "),
+        choices: ["python", "javaScript", "nextjs", "c++"] // javascript
+    },
+    {
+        name: "question_9",
+        type: "list",
+        message: chalk.blueBright.bold("Q9.What are the three main 'simple types' in TypeScript? "),
+        choices: ["Array,Object,Boolean", "Object,Array,Symbol", "Object,String,Number", "String,Number,Boolean"] // string,number,boolean
+    },
+    {
+        name: "question_10",
+        type: "list",
+        message: chalk.blueBright.bold("Q10.TypeScript can always correctly infer a variables type? "),
+        choices: ["true", "false", "yes", "no"] // Number
+    },
+]);
+let score = 0;
+switch (quiz.question_1) {
+    case ".ts":
+        console.log(chalk.redBright.bold("\t\t1. Correct!"));
+        ++score;
+        break;
+    default:
+        console.log(chalk.magentaBright.bold("\t\t1. Incorrect!"));
+}
+switch (quiz.question_2) {
+    case "JavaScript":
+        console.log(chalk.redBright.bold("\t\t2. Correct!"));
+        ++score;
+        break;
+    default:
+        console.log(chalk.magentaBright.bold("\t\t2. Incorrect!"));
+}
+switch (quiz.question_3) {
+    case "TypeScript Compiler (tsc)":
+        console.log(chalk.redBright.bold("\t\t3. Correct!"));
+        ++score;
+        break;
+    default:
+        console.log(chalk.magentaBright.bold("\t\t3. Incorrect!"));
+}
+switch (quiz.question_4) {
+    case "Both A and C":
+        console.log(chalk.redBright.bold("\t\t4. Correct!"));
+        ++score;
+        break;
+    default:
+        console.log(chalk.magentaBright.bold("\t\t4. Incorrect!"));
+}
+switch (quiz.question_5) {
+    case "Number":
+        console.log(chalk.redBright.bold("\t\t5. Correct!"));
+        ++score;
+        break;
+    default:
+        console.log(chalk.magentaBright.bold("\t\t5. Incorrect!"));
+}
+switch (quiz.question_6) {
+    case "String":
+        console.log(chalk.redBright.bold("\t\t6. Correct!"));
+        ++score;
+        break;
+    default:
+        console.log(chalk.magentaBright.bold("\t\t6. Incorrect!"));
+}
+switch (quiz.question_7) {
+    case "boolean":
+        console.log(chalk.redBright.bold("\t\t7. Correct!"));
+        ++score;
+        break;
+    default:
+        console.log(chalk.magentaBright.bold("\t\t7. Incorrect!"));
+}
+switch (quiz.question_8) {
+    case "javaScript":
+        console.log(chalk.redBright.bold("\t\t8. Correct!"));
+        ++score;
+        break;
+    default:
+        console.log(chalk.magentaBright.bold("\t\t8. Incorrect!"));
+}
+switch (quiz.question_9) {
+    case "String,Number,Boolean":
+        console.log(chalk.redBright.bold("\t\t9. Correct!"));
+        ++score;
+        break;
+    default:
+        console.log(chalk.magentaBright.bold("\t\t9. Incorrect!"));
+}
+switch (quiz.question_10) {
+    case "false":
+        console.log(chalk.redBright.bold("\t\t10. Correct!"));
+        ++score;
+        break;
+    default:
+        console.log(chalk.magentaBright.bold("\t\t10. Incorrect!"));
+}
+console.log(chalk.yellowBright.bold.underline(`\t\t${user.name} Your score is ${score}/10`));
+console.log(chalk.redBright.bgWhiteBright.bold("\t\tThank you for taking the quiz!"));
--- Quiz.ts
+++ Quiz.ts
@@ -1,44 +1,183 @@
 #! /usr/bin/env node
 
-import chalk from "chalk";
 import inquirer from "inquirer";
+import chalk from "chalk";
 
-const apiLink:string = "https://opentdb.com/api.php?amount=6&category=18&difficulty=easy&type=multiple";
 
-let fetchData = async (data:string) => {
-    let fetchQuiz:any = await fetch(data);
-    let res = await fetchQuiz.json();
-    return res.results;
-};
-
-let data = await fetchData(apiLink);
-
-let startQuiz = async () => {
-    let score: number = 0;
-    // for user name
-    let name = await inquirer.prompt({
+const user = await inquirer.prompt([
+    {
+        name: "name",
         type: "input",
-        name: "fname",
-        message: "What is Your Name?"
-    });
+        message: chalk.greenBright.bold("What's your name?")
+    },
+]);
 
-    for(let i = 1 ; i< 5 ; i++){
-        let answers = [...data[i].incorrect_answers, data[i].correct_answer];
+console.log(chalk.yellowBright.underline(`\t\tWelcome ${user.name} to the TypeScript Quiz!`));
+console.log(chalk.yellowBright.underline("\t\tPlease answer the following questions to the best of your ability."));
+console.log(chalk.yellowBright.underline("\t\tLet's start the quiz!"));
 
-        let ans = await inquirer.prompt({
-            type: "list",
-            name: "quiz",
-            message:data[i].question,
-            choices: answers.map((val:any)=> val),
-        });
-        if(ans.quiz == data[i].correct_answer){
-            ++score
-            console.log(chalk.bold.italic.blue("Correct"))
-        }else{
-            console.log(`Correct answer is ${chalk.bold.italic.red(data[i].correct_answer)}`)
-        }
-    }
-    console.log(`Dear ${chalk.greenBright.bold(name.fname)}, Your Score is ${chalk.red.bold(score)}, Out of ${chalk.red.bold('5')}`)
-};       
 
-    startQuiz();
+const quiz: {
+    question_1: string;
+    question_2: string;
+    question_3: string;
+    question_4: string;
+    question_5: string;
+    question_6: string;
+    question_7: string;
+    question_8: string;
+    question_9: string;
+    question_10: string;
+} = await inquirer.prompt([
+    {
+        name: "question_1",
+        type: "list",
+        message: chalk.blueBright.bold("Q1.What is the file extension for TypeScript files?"),
+        choices: [".js", ".ts", ".html", ".css"]   // .ts
+    },
+    {
+        name: "question_2",
+        type: "list",
+        message:chalk.blueBright.bold( "Q2.TypeScript code is compiled into:"),
+        choices: ["Machine code", "Assembly code", "JavaScript", "TypeScript bytecode"]   // Javascript
+    },
+    {
+        name: "question_3",
+        type: "list",
+        message: chalk.blueBright.bold("Q3.Which tool is commonly used to transpile TypeScript code to JavaScript?"),
+        choices: ["Babel", "Webpack", "TypeScript Compiler (tsc)", "Node.js"]   // TypeScript Compiler (tsc)
+    },
+    {
+        name: "question_4",
+        type: "list",
+        message: chalk.blueBright.bold("Q4.Which of these are supported in TypeScript?"),
+        choices: ["Number", "Integar", "Any", "Both A and C", "All of the above"]   // Both A and C
+    },
+    {
+        name: "question_5",
+        type: "list",
+        message: chalk.blueBright.bold("Q5.Consider the code (let age = 25;). What is the data type of the variable age?" ),
+        choices: ["Number", "Any", "String", "boolean"]   // Number
+    },
+    {
+        name: "question_6",
+        type: "list",
+        message: chalk.blueBright.bold("Q6.Consider the code (let myName= 'kanwal bilal';). What is the data type of the variable myName? "),
+        choices: ["Number", "Any", "String", "boolean"]   // string
+    },
+    {
+        name: "question_7",
+        type: "list",
+        message: chalk.blueBright.bold("Q7.Consider the code (let isMarried = true;). What is the data type of the variable isMarried? "),
+        choices: ["Number", "Any", "String", "boolean"]   // boolean
+    },
+    {
+        name: "question_8",
+        type: "list",
+        message: chalk.blueBright.bold("Q8.Typescript is the superset of? "),
+        choices: ["python", "javaScript", "nextjs", "c++"]   // javascript
+    },
+    {
+        name: "question_9",
+        type: "list",
+        message: chalk.blueBright.bold("Q9.What are the three main 'simple types' in TypeScript? "),
+        choices: ["Array,Object,Boolean", "Object,Array,Symbol", "Object,String,Number", "String,Number,Boolean"]   // string,number,boolean
+    },
+    {
+        name: "question_10",
+        type: "list",
+        message:chalk.blueBright.bold( "Q10.TypeScript can always correctly infer a variables type? "),
+        choices: ["true", "false", "yes", "no"]   // Number
+    },
+]);
+
+let score: number = 0;
+
+switch(quiz.question_1) {
+    case ".ts":
+        console.log(chalk.redBright.bold("\t\t1. Correct!"));
+        ++score;
+        break;
+        default:
+            console.log(chalk.magentaBright.bold("\t\t1. Incorrect!"));
+}
+
+switch(quiz.question_2) {
+    case "JavaScript":
+        console.log(chalk.redBright.bold("\t\t2. Correct!"));
+        ++score;
+        break;
+        default:
+            console.log(chalk.magentaBright.bold("\t\t2. Incorrect!"));
+}
+
+switch(quiz.question_3) {
+    case "TypeScript Compiler (tsc)":
+        console.log(chalk.redBright.bold("\t\t3. Correct!"));
+        ++score;
+        break;
+        default:
+            console.log(chalk.magentaBright.bold("\t\t3. Incorrect!"));
+}
+
+switch(quiz.question_4) {
+    case "Both A and C":
+        console.log(chalk.redBright.bold("\t\t4. Correct!"));
+        ++score;
+        break;
+        default:
+            console.log(chalk.magentaBright.bold("\t\t4. Incorrect!"));
+}
+
+switch(quiz.question_5) {
+    case "Number":
+        console.log(chalk.redBright.bold("\t\t5. Correct!"));
+        ++score;
+        break;
+        default:
+            console.log(chalk.magentaBright.bold("\t\t5. Incorrect!"));
+}
+switch(quiz.question_6) {
+    case "String":
+        console.log(chalk.redBright.bold("\t\t6. Correct!"));
+        ++score;
+        break;
+        default:
+            console.log(chalk.magentaBright.bold("\t\t6. Incorrect!"));
+}
+switch(quiz.question_7) {
+    case "boolean":
+        console.log(chalk.redBright.bold("\t\t7. Correct!"));
+        ++score;
+        break;
+        default:
+            console.log(chalk.magentaBright.bold("\t\t7. Incorrect!"));
+}
+switch(quiz.question_8) {
+    case "javaScript":
+        console.log(chalk.redBright.bold("\t\t8. Correct!"));
+        ++score;
+        break;
+        default:
+            console.log(chalk.magentaBright.bold("\t\t8. Incorrect!"));
+}
+switch(quiz.question_9) {
+    case "String,Number,Boolean":
+        console.log(chalk.redBright.bold("\t\t9. Correct!"));
+        ++score;
+        break;
+        default:
+            console.log(chalk.magentaBright.bold("\t\t9. Incorrect!"));
+}
+switch(quiz.question_10) {
+    case "false":
+        console.log(chalk.redBright.bold("\t\t10. Correct!"));
+        ++score;
+        break;
+        default:
+            console.log(chalk.magentaBright.bold("\t\t10. Incorrect!"));
+}
+
+console.log(chalk.yellowBright.bold.underline(`\t\t${user.name} Your score is ${score}/10`));
+console.log(chalk.redBright.bgWhiteBright.bold("\t\tThank you for taking the quiz!"));
+    
